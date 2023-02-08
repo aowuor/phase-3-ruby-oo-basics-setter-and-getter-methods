16b035e986289ead5c060d0dd6791551958ba23c
aowuor <angelaowuor@gmail.com>
2023-02-08 22:15:04 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+    #setter methods
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    #getter methods
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+child = Person.new
+child.name = "Ann"
+child.job = "student"
+
+puts child.name
+puts child.job
