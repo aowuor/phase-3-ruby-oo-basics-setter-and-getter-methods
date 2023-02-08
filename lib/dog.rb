8bee5497254834a24310efdf668c1c45d15c2567
aowuor <angelaowuor@gmail.com>
2023-02-08 22:14:17 +0300

    commit tree c932038985e2d9f5c7fb8c9bbc132039ee1486e4

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
 
 end
+
+mastiff = Dog.new
+mastiff.name = "Mastiff"
+mastiff.breed = "Small"
+puts mastiff.name
+puts mastiff.breed
